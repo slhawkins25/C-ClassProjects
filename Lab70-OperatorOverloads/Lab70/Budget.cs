71bc3a2eda076d3c6a3cb73862927e48dc9c9a04
Savanna <savannalhawkins@gmail.com>
2017-09-27 16:30:52 -0400

    Add files via upload

---
+++ Lab70-OperatorOverloads/Lab70/Budget.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab70
+{
+    class Budget
+    {
+        public decimal Revenue { get; set; }
+        public decimal Expenses { get; set; }
+
+        public Budget()
+        {
+        }
+
+        public Budget(decimal revenue, decimal expenses)
+        {
+            Revenue = revenue;
+            Expenses = expenses;
+        }
+
+        // 'true' operator ensures Budget properties are not zero.
+        public static bool operator true(Budget budget)
+        {
+            if (budget.Revenue == 0 && budget.Expenses == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        // 'false' operator only required if 'true' operator is present.
+        public static bool operator false(Budget budget)
+        {
+            return false;
+        }
+
+        // '+' overload adds Budget objects.
+        public static Budget operator +(Budget a, Budget b)
+        {
+            Budget sum = new Budget();
+            sum.Revenue = a.Revenue + b.Revenue;
+            sum.Expenses = a.Expenses + b.Expenses;
+            return sum;
+        }
+
+        // Show Budget properties.
+        public void ShowBudget(string title)
+        {
+            Console.WriteLine(title);
+            Console.WriteLine("Revenue: " + Revenue.ToString("C"));
+            Console.WriteLine("Expenses: " + Expenses.ToString("C"));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab70-OperatorOverloads/Lab70/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal revenue = 1043000m, expenses = 102030m;
+            decimal revenue1 = 2043000m, expenses1 = 202030m;
+
+            Budget retail = new Budget(revenue, expenses); // Create budget.
+            retail.ShowBudget("Brick & Mortar Budget:");
+
+            Budget online = new Budget(revenue1, expenses1); // Create budget.
+            online.ShowBudget("Online Budget:");
+
+            Budget combined = retail + online;             // Combine budgets.
+            combined.ShowBudget("Brick & Mortar and Online Budgets Combined:");
+            //Budget emptyBudget = new Budget(0,0);             // Create budget.
+
+            Budget emptyBudget = new Budget();             // Create budget.
+            if (emptyBudget)                              // Ensure balances
+            {                                            // are not zero.
+                emptyBudget.ShowBudget("Budget is not empty.");
+            }
+            else
+            {
+                Console.WriteLine("Revenue = {0}", emptyBudget.Revenue);
+                Console.WriteLine("Expenses = {0}", emptyBudget.Expenses);
+                Console.WriteLine("Empty Budget.");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
