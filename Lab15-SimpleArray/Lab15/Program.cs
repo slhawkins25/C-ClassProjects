7074e9eccaa98d720a61d71321e28e5e1e58b452
Savanna <savannalhawkins@gmail.com>
2017-09-27 15:57:10 -0400

    Add files via upload

---
+++ Lab15-SimpleArray/Lab15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class Program
+    {
+       private static int[] ints = { 0, 1, 3, 5, 7 }; // Field must be static in order to use in Main method without an instance
+       private static int i = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The value is {0}", ints[0]);
+            Console.WriteLine("The variable i is equal to: {0}", i);
+            Console.WriteLine("I'm ready to call your method.");
+
+            processInts(ints, i);
+        }
+        static void processInts(int[] ints, int theInteger)
+        {
+           ints[0] = 100;
+           int i = 10;
+
+            Console.WriteLine("The value is {0}", ints[0]);
+            Console.WriteLine("The variable i is equal to: {0}", i);
+
+        }
+    }
+}
+
+
---
+++ Lab16-Bools/Lab16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            // int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            bool isLessThan10 = userNumber <  10; // one predicate
+            bool isBetween0And5 = (userNumber >=  0) && (userNumber <=  5);  // two predicates make one logical condition
+
+            Console.WriteLine("Integer less than 10? {0}", isLessThan10);
+            Console.WriteLine("Integer between 0 and 5? {0}", isBetween0And5);
+            Console.WriteLine("Exactly one of the above is true? {0}", isLessThan10 ^ isBetween0And5); // ^ Exclusive OR
+
+            Console.ReadLine();
+
+        }
+    }
+}
