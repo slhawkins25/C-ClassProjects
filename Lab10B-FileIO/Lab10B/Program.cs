c858e674fa0532cead51948e2b4ca624c568e38c
Savanna <savannalhawkins@gmail.com>
2017-09-27 13:14:31 -0400

    Add files via upload

---
+++ Lab10B-FileIO/Lab10B/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab10B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doWork();
+        }
+
+
+        static void doWork()
+        {
+            FileStream myFileStream = new FileStream("test.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            try
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    Console.WriteLine("Writing value: {0}", i);
+                    myFileStream.WriteByte((byte)i);
+                }
+
+                myFileStream.Position = 0;
+
+                for (int value; (value = myFileStream.ReadByte()) > -1;)
+                {
+                    Console.WriteLine("Read Value: {0}", value);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                myFileStream.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Lab10G-FileIO/Lab10G/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab10G
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("Finally block reached");
+            }
+        }
+
+        static void doWork()
+        {
+            if (File.Exists("test.txt"))
+            {
+                File.Delete("test.txt");
+                Console.WriteLine("The file test.txt was deleted.");
+            }
+        }
+    }
+}
